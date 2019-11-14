e71b9abd1e4766dec1c9e028efa3372f40d4b76a
Richard Souza <richard.souza@rangu.com.br>
2019-11-14 16:57:51 -0300

    project base

---
+++ src/main/java/br/com/rich/springreactivesecurityoauth2/SpringReactiveSecurityOauth2Application.java
@@ -0,0 +1,13 @@
+package br.com.rich.springreactivesecurityoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveSecurityOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveSecurityOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rich/springreactivesecurityoauth2/SpringReactiveSecurityOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rich.springreactivesecurityoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveSecurityOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
